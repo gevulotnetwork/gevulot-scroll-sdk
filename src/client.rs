58243531183363ab3cba87b7693643df779e82ae
Andrzej Broński <andrzej1_1@o2.pl>
2024-11-21 14:19:33 +0000

    Copy implementation from prototype repository.

---
+++ src/client.rs
@@ -0,0 +1,97 @@
+use async_trait::async_trait;
+
+use crate::{
+  GetVkRequest, GetVkResponse, ProveRequest, ProveResponse, QueryTaskRequest, QueryTaskResponse,
+};
+
+const DEFAULT_PROVING_SERVICE_URL: &str = "http://scroll-proving-service.gevulot.com:3000";
+
+pub struct Client {
+  proving_service_url: String,
+  auth_token: String,
+}
+
+impl Client {
+  pub fn new(auth_token: Option<String>) -> Self {
+    Client {
+      proving_service_url: DEFAULT_PROVING_SERVICE_URL.to_string(),
+      auth_token: auth_token.unwrap_or("".to_string()),
+    }
+  }
+
+  pub async fn is_healthy(&self) -> bool {
+    let client = reqwest::Client::new();
+    let endpoint = self.proving_service_url.clone() + "/health";
+    let response = client.get(endpoint).send().await.unwrap();
+
+    response.text().await.unwrap() == "OK"
+  }
+}
+
+#[async_trait]
+impl ProvingService for Client {
+  fn is_local(&self) -> bool {
+    // NOTE: Saying this is local service is a dirty way to prevent running
+    // Scroll Proving SDK with multiple workers configured - which makes
+    // no sense here.
+    true
+  }
+
+  async fn get_vk(&self, req: GetVkRequest) -> GetVkResponse {
+    // Construct the URL, including query parameters.
+    let client = reqwest::Client::new();
+    let endpoint = self.proving_service_url.clone() + "/get_vk";
+    let response = client
+      .get(endpoint)
+      .query(&[
+        ("circuit_type", req.circuit_type),
+        ("circuit_version", req.circuit_version),
+      ])
+      .send()
+      .await
+      .unwrap();
+
+    let vk_response: GetVkResponse = response.json().await.unwrap();
+    vk_response
+  }
+
+  async fn prove(&self, req: ProveRequest) -> ProveResponse {
+    // Construct the URL, including query parameters.
+    let client = reqwest::Client::new();
+    let endpoint = self.proving_service_url.clone() + "/prove_task";
+    let response = client
+      .post(endpoint)
+      .json(&req)
+      .bearer_auth(&self.auth_token) // NOTE: Auth token is required to submit a new task.
+      .send()
+      .await
+      .unwrap();
+
+    let prove_response: ProveResponse = response.json().await.unwrap();
+    prove_response
+  }
+
+  async fn query_task(&self, req: QueryTaskRequest) -> QueryTaskResponse {
+    // Construct the URL, including query parameters.
+    let client = reqwest::Client::new();
+    let endpoint = self.proving_service_url.clone() + "/query_task";
+    let response = client
+      .get(endpoint)
+      .query(&[("task_id", req.task_id)])
+      .send()
+      .await
+      .unwrap();
+
+    let query_task_response: QueryTaskResponse = response.json().await.unwrap();
+    query_task_response
+  }
+}
+
+// Interface from the Scroll Proving SDK.
+#[async_trait]
+pub trait ProvingService {
+  fn is_local(&self) -> bool;
+  async fn get_vk(&self, req: GetVkRequest) -> GetVkResponse;
+  async fn prove(&self, req: ProveRequest) -> ProveResponse;
+  async fn query_task(&self, req: QueryTaskRequest) -> QueryTaskResponse;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,148 @@
+use std::fmt;
 
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+// Supported circuit versions that SHOULD be supported by proving service.
+pub const CIRCUIT_VERSION_0_13: &str = "v0.13.1";
+
+#[cfg(feature = "client")]
+pub mod client;
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct GetVkRequest {
+  pub circuit_type: String,
+  pub circuit_version: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct GetVkResponse {
+  pub vk: String,
+  pub error: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ProveRequest {
+  pub circuit_type: String,
+  pub circuit_version: String,
+  pub hard_fork_name: String,
+  pub input: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ProveResponse {
+  pub task_id: String,
+  pub circuit_type: String,
+  pub circuit_version: String,
+  pub hard_fork_name: String,
+  pub status: TaskStatus,
+  pub created_at: f64,
+  pub started_at: Option<f64>,
+  pub finished_at: Option<f64>,
+  pub compute_time_sec: Option<f64>,
+  pub input: Option<String>,
+  pub proof: Option<String>,
+  pub vk: Option<String>,
+  pub error: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct QueryTaskRequest {
+  pub task_id: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct QueryTaskResponse {
+  pub task_id: String,
+  pub circuit_type: String,
+  pub circuit_version: String,
+  pub hard_fork_name: String,
+  pub status: TaskStatus,
+  pub created_at: f64,
+  pub started_at: Option<f64>,
+  pub finished_at: Option<f64>,
+  pub compute_time_sec: Option<f64>,
+  pub input: Option<String>,
+  pub proof: Option<String>,
+  pub vk: Option<String>,
+  pub error: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug, PartialEq, Clone)]
+pub enum TaskStatus {
+  Queued,
+  Proving,
+  Success,
+  Failed,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum CircuitType {
+  Undefined,
+  Chunk,
+  Batch,
+  Bundle,
+}
+
+impl CircuitType {
+  pub fn from_u8(v: u8) -> Self {
+    match v {
+      1 => CircuitType::Chunk,
+      2 => CircuitType::Batch,
+      3 => CircuitType::Bundle,
+      _ => CircuitType::Undefined,
+    }
+  }
+
+  pub fn to_u8(self) -> u8 {
+    match self {
+      CircuitType::Undefined => 0,
+      CircuitType::Chunk => 1,
+      CircuitType::Batch => 2,
+      CircuitType::Bundle => 3,
+    }
+  }
+
+  pub fn from_string(value: &str) -> Self {
+    match value {
+      "chunk" => Self::Chunk,
+      "bundle" => Self::Bundle,
+      "batch" => Self::Batch,
+      _ => Self::Undefined,
+    }
+  }
+}
+
+impl fmt::Display for CircuitType {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      Self::Chunk => write!(f, "chunk"),
+      Self::Batch => write!(f, "batch"),
+      Self::Bundle => write!(f, "bundle"),
+      Self::Undefined => write!(f, "undefined"),
+    }
+  }
+}
+
+impl Serialize for CircuitType {
+  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+  where
+    S: Serializer,
+  {
+    match *self {
+      CircuitType::Undefined => serializer.serialize_u8(0),
+      CircuitType::Chunk => serializer.serialize_u8(1),
+      CircuitType::Batch => serializer.serialize_u8(2),
+      CircuitType::Bundle => serializer.serialize_u8(3),
+    }
+  }
+}
+
+impl<'de> Deserialize<'de> for CircuitType {
+  fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+  where
+    D: Deserializer<'de>,
+  {
+    let v: u8 = u8::deserialize(deserializer)?;
+    Ok(CircuitType::from_u8(v))
+  }
+}
